bce82852f6f72892b42523ec772bdc8a4268bfbb
Ritam Basu <67322468+alternativeritam@users.noreply.github.com>
2026-02-20 16:52:00 +0530

    Inorder using recursion

--- Inordore.cpp
+++ Inordore.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+struct Node{
+
+	int data;
+	Node *left;
+	Node *right;
+	Node(int val){
+		data=val;
+		left=NULL;
+		right=NULL;
+	}
+};
+
+void Inorder(Node *node){
+	if(node==NULL) return;
+	Inorder(node->left);
+	cout<<node->data<<endl;
+	Inorder(node->right);
+
+}
+
+int main(){
+
+	// build the tree
+	Node *node  = new Node(1);
+	Inorder(node);
+	return 0;
+}
